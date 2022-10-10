28568fe5b17c543e45dccbb5069f1f6207b28ecd
Diego R <diego-roney@outlook.com>
2022-10-10 20:26:26 -0300

    indo bem

---
+++ info.md
@@ -0,0 +1,3 @@
+https://blog.jetbrains.com/webstorm/2021/10/building-a-blog-with-next-js-and-mdx/#What_well_be_building
+
+npm run dev
---
+++ pages/bio.js
@@ -0,0 +1,9 @@
+const Bio = () => {
+    return (
+      <div className="mt-3">
+        <p className="display-4 text-center">Eu sou Diego Roney</p>
+        <p className="text-center">colocar minha Bio aqui</p>
+      </div>
+    )
+  }
+  export default Bio
