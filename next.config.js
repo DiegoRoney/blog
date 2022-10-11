15c7e94900054cf4d2456660abc12b2c304c049e
Diego R <diego-roney@outlook.com>
2022-10-11 20:29:44 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,9 +7,22 @@
 // module.exports = nextConfig
 
 
-const isProd = process.env.NODE_ENV === 'production'
+
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
-}
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `nextjs-github-pages` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+  images: {
+    unoptimized: true,
+  },
+};
