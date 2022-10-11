1f9cc8509d6268fb3fda5050b78bdd3771b06e95
Diego R <diego-roney@outlook.com>
2022-10-11 09:59:32 -0300

    Update bio.js

--- pages/bio.js
+++ pages/bio.js
@@ -2,7 +2,7 @@ const Bio = () => {
     return (
       <div className="mt-3">
         <p className="display-4 text-center">Eu sou Diego Roney</p>
-        <p className="text-center">colocar minha Bio aqui</p>
+        <p className="text-center">colocar minha Bio aqui !</p>
       </div>
     )
   }
