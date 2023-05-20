0758adc33f02334f5f098d07d59ae9b18faf9ecf
Diego R <46231316+DiegoRoney@users.noreply.github.com>
2023-05-20 12:03:05 -0300

    add

--- pages/bio.js
+++ pages/bio.js
@@ -1,9 +1,29 @@
 const Bio = () => {
-    return (
-      <div className="mt-3">
-        <p className="display-4 text-center">Eu sou Diego Roney</p>
-        <p className="text-center">colocar minha Bio aqui !</p>
-      </div>
-    )
-  }
-  export default Bio
+  return (
+    <div className="mt-3">
+      <p className="display-4 text-center">Eu sou Diego!</p>
+      <p className="text-center">
+        É com muita satisfação que apresentamos Diego, um talentoso profissional
+        apaixonado por criar experiências digitais incríveis. Como desenvolvedor
+        Front-End, Diego possui habilidades excepcionais na criação de
+        interfaces interativas e responsivas que cativam e encantam os usuários.
+        Além disso, Diego também é um mestre no uso do pacote Adobe, dominando
+        ferramentas essenciais para edição de fotos e vídeos. Sua criatividade e
+        olhar apurado garantem resultados visualmente impressionantes,
+        transformando ideias em imagens fascinantes. Combinando suas habilidades
+        técnicas e visuais, Diego eleva projetos a outro nível, agregando valor
+        estético e funcionalidade aos produtos finais. Sua dedicação e
+        compromisso em entregar resultados de alta qualidade fazem dele um
+        parceiro de equipe excepcional. Estamos verdadeiramente empolgados por
+        ter Diego fazendo parte da nossa equipe. Sua paixão pela criação e sua
+        expertise no desenvolvimento front-end e design tornam-no um ativo
+        inestimável para qualquer projeto. Se você está em busca de uma mente
+        criativa e talentosa para impulsionar seu próximo projeto, não hesite em
+        entrar em contato com Diego. Com sua experiência e dedicação, ele
+        certamente trará vida e beleza às suas ideias. Juntos, vamos transformar
+        o mundo digital em um lugar mais belo e funcional!
+      </p>
+    </div>
+  );
+};
+export default Bio;
